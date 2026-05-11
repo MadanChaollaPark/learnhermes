00cf3a098d3bc7171d6cf908d7e5af7583e22db5
Madan Park <madan_chaolla@yahoo.co.in>
2026-05-11 17:28:37 +0200

    test(mocks): add barrel re-exports for mocks/

---
+++ tests/mocks/index.ts
@@ -0,0 +1,6 @@
+export { FakeClock } from "./clock";
+export { MockChannel } from "./channel";
+export type { SentMessage } from "./channel";
+export { MockAIClient } from "./ai-client";
+export { makeWorkspace, withWorkspace, makeSystemWorkspace } from "./fs-helpers";
+export type { Workspace } from "./fs-helpers";
